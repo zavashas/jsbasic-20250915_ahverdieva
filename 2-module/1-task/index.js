08371496e043502df1e6c67e0f420f9901a54a0c
zavashas <simazavadskis2808@gmail.com>
2025-09-24 23:34:40 +0300

    занятие 2 +Функция sumSalary(salaries)

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,11 @@
 function sumSalary(salaries) {
-  // ваш код...
+  let sum = 0;
+
+  for (let k in salaries) {
+    if (Number.isFinite(salaries[k])) {
+      sum += salaries[k];
+    }
+  }
+
+  return sum;
 }
