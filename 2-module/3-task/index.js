ae437fb39fbea2413bde5c67e9010d864cf7ae38
zavashas <simazavadskis2808@gmail.com>
2025-09-24 23:49:41 +0300

    занятие 2 +калькулятор

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,5 +1,14 @@
 let calculator = {
-  // ваш код
+  read(a, b) {
+    this.num1 = a;
+    this.num2 = b;
+  },
+  sum() {
+    return this.num1 + this.num2;
+  },
+  mul() {
+    return this.num1 * this.num2;
+  },
 };
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
